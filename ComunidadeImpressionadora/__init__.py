5477f01a361189f18927ed9fc8428a9b92d78b23
Leandro <137462325+Leoprog23@users.noreply.github.com>
2024-01-29 19:24:02 -0300

    Update __init__.py

--- ComunidadeImpressionadora/__init__.py
+++ ComunidadeImpressionadora/__init__.py
@@ -11,7 +11,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 import os
-
+import sqlalchemy
 
 # Colocando o site no ar:
     # Método que tem dentro da Classe Flask, cria o site:
@@ -31,5 +31,17 @@ criptografia = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'alert-info'
+
+from ComunidadeImpressionadora import models
+
+engine = sqlalchemy.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+inspector = sqlalchemy.inspect(engine)
+if not inspector.has_table('usuario'):
+    with app.app_context():
+        Database.drop_all()
+        Database.create_all()
+        print('Base de dados criada')
+else:
+    print('Base de dados já existente')
 
 from ComunidadeImpressionadora import routes
